5f506803a12f654237f5e492b6e56374a5df1055
mdbabaylan <matmatmark@gmail.com>
2023-10-22 21:13:44 +0800

    initial commit with gym card done

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,71 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app>
+    <v-app-bar color="deep-purple accent-4" dark prominent>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>ダッシュボード</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" absolute bottom temporary>
+      <v-list nav dense>
+        <v-list-item-group v-model="group" active-class="deep-purple--text text--accent-4">
+          <v-list-item>
+            <v-list-item-title>Foo</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Bar</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Fizz</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Buzz</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+
+    <v-main> <!-- dump child components here, I'm assuming -->
+      <v-container fluid>
+        <GymPrograms />
+        BTC and ETH price feed
+        Gym and calorie reminder, if Date == Monday => Pushday + Calories
+        RSS / Show updates of manga you are reading
+
+        responsive, simple and clean design
+        dark theme power pls
+        that is all. thanks
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import GymPrograms from "./components/GymPrograms";
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
+    GymPrograms
+  },
+
+  data: () => ({
+    drawer: false,
+    group: null,
+  }),
+
+
+  watch: {
+    group() {
+      this.drawer = false
+    },
+  },
+
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
